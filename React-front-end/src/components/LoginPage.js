43fa52cb56e31a9178fa9488b045401294b2c58b
georgio-nehme <georgionehme@gmail.com>
2022-06-26 09:45:09 +0300

    Completed the Register and Login  fron to back

--- React-front-end/src/components/LoginPage.js
+++ React-front-end/src/components/LoginPage.js
@@ -1,13 +1,43 @@
+import React , { useState } from "react";
+import { useLocation ,useNavigate } from "react-router-dom";
+
 const LoginPage = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    async function login() {
+        let user = {email, password};
+        console.log(user);
+        let result = await fetch("http://localhost:5000/users/login",{ 
+            method:'POST',
+            body:JSON.stringify(user),
+            headers:{
+                "Content-Type": "Application/json",
+                "Accept":"Application/json"
+            }
+        
+        })
+        result = await(result) 
+        console.log("result",result)
+
+    };
+
+
+
+    
+    
     return ( 
         <div className="login_page">
             <h1>Login</h1>
-            <input placeholder="Email"></input>
-            <input placeholder="Password" type="password"></input>
-            <button>Login</button>
+            <input placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
+            <input placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}type="password"></input>
+            <button onClick={login}>Login</button>
             
         </div>
-     );
+    );
+
 }
+
  
 export default LoginPage;
--- React-front-end/src/components/RegisterPage.js
+++ React-front-end/src/components/RegisterPage.js
@@ -1,11 +1,39 @@
+import React , { useState } from "react";
+
+
+
 const Register = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function signUp() 
+    {
+        let newUser = { name , email, password};
+        console.log(newUser);
+        let result = await fetch("http://localhost:5000/users/register",{ 
+            method:'POST',
+            body:JSON.stringify(newUser),
+            headers:{
+                "Content-Type": "Application/json",
+                "Accept":"Application/json"
+            }
+        
+        })
+        result = await result.json()
+        console.log("result",result)
+        localStorage.setItem("user_id", JSON.stringify(result))
+    };
+
+
     return (  
         <div className="register">
             <h1>Register</h1>
-            <input placeholder="Name"></input>
-            <input placeholder="Email"></input>
-            <input placeholder="Password" type="password"></input>
-            <button>Register</button>
+            <input placeholder="Name" value={name} onChange={(e)=>setName(e.target.value)}></input>
+            <input placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
+            <input placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}type="password"></input>
+            <button onClick={signUp}>Register</button>
         </div>
      );
 }
