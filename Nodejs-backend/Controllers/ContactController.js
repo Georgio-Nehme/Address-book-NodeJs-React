93bc9f39b0225ddbbaa5b95c3bd3d53e642f846f
georgio-nehme <georgionehme@gmail.com>
2022-06-26 11:26:59 +0300

    Created and tested the Api to retrieve the contacts of a user

--- Nodejs-backend/Controllers/ContactController.js
+++ Nodejs-backend/Controllers/ContactController.js
@@ -1,18 +1,26 @@
 const Contact = require('../Models/Contact');
-const User = require('../Models/User');
+// const User = require('../Models/User');
 
-async function getAllContacts() {
-    return await Contact.find();
+async function getAllContacts(req, res) {
+  Contact.find({user_id : req.body.user_id})
+  .then(data => {
+    res.send(data);
+  })
+  .catch(err => {
+    res.status(500).send({
+      message:
+        err.message || "Some error occurred while retrieving contacts."
+    });
+  });
 }
   
-
+// Create and Save a new contact
 async function addContact(req,res) { 
-   // Create and Save a new contact
+
     if (!req.body.name) {
       res.status(400).send({ message: "Contact should have a name"});
       return;
     }
-  
     // Create a contact
     const contact = await new Contact({
       name: req.body.name,
@@ -22,7 +30,6 @@ async function addContact(req,res) {
       location: req.body.location,
       user_id: req.body.user_id
     });
-  
     // Save Contact in the database
     contact.save(contact)
     .then(data => {
