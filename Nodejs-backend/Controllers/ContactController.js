ee26570aa5ff161c560c0201bfa18619d2ef290f
georgio-nehme <georgionehme@gmail.com>
2022-06-26 10:22:48 +0300

    Created and tested the add contact Api

--- Nodejs-backend/Controllers/ContactController.js
+++ Nodejs-backend/Controllers/ContactController.js
@@ -1,4 +1,5 @@
 const Contact = require('../Models/Contact');
+const User = require('../Models/User');
 
 async function getAllContacts() {
     return await Contact.find();
@@ -6,23 +7,37 @@ async function getAllContacts() {
   
 
 async function addContact(req,res) { 
-    try{    
-        const {
-            name,
-            email,
-            relationship,
-            phone,
-            location,
-            user_id
-        } = body;
-
-        const contact = new Contact({body
-            });
-            return await contact.save();
-        } catch(error) {
-    return(error)
+   // Create and Save a new contact
+    if (!req.body.name) {
+      res.status(400).send({ message: "Contact should have a name"});
+      return;
     }
-}
+  
+    // Create a contact
+    const contact = await new Contact({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      relationship: req.body.relationship,
+      location: req.body.location,
+      user_id: req.body.user_id
+    });
+  
+    // Save Contact in the database
+    contact.save(contact)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Contact."
+      });
+    });
+  };
+
+
+
 
 
 async function deleteContact() {
